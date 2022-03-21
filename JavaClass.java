10a28db639ec1268b3aaaea1c3a5e835910ecfb2
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-03-21 08:48:54 +0500

    first

---
+++ JavaClass.java
@@ -0,0 +1,13 @@
+public class JavaClass {
+   public static void main(String[] args) {
+      System.out.print("Hello Java\n");
+      System.out.print("Hello Java\n");
+
+      int x, y;
+      x= 20;
+      y= 15;
+      System.out.print(x+y);
+
+
+   }
+}
