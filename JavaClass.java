06faec3d0b0e3e84cd0bcba64ce97040e6f295cc
Saeed-Afzal <65010428+Saeed-Afzal@users.noreply.github.com>
2022-03-22 09:00:14 +0500

    loops

--- JavaClass.java
+++ JavaClass.java
@@ -1,6 +1,8 @@
 package firstPrograminJava;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane; // use for GUI
+import java.util.Random; // use for random number
+import java.util.Scanner; // use for input
 
 public class JavaClass {
    public static void main(String[] args) {
@@ -12,21 +14,88 @@ public class JavaClass {
       // y= 15;
       // System.out.print(x+y);
 
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("What is your name");
-      String name = scanner.nextLine();
+      // Scanner scanner = new Scanner(System.in);
+      // System.out.println("What is your name");
+      // String name = scanner.nextLine();
 
-      System.out.println("What is your age");
-      int age = scanner.nextInt();
-      scanner.nextLine(); // use to stay for next input 
+      // System.out.println("What is your age");
+      // int age = scanner.nextInt();
+      // scanner.nextLine(); // use to stay for next input
 
-      System.out.println("What is your favourite food");
-      String food = scanner.nextLine();
+      // System.out.println("What is your favourite food");
+      // String food = scanner.nextLine();
+
+      // System.out.println("Hello " + name);
+      // System.out.println("Hello " + age);
+      // System.out.println("Hello " + food);
+
+      // GUI starts
+      // String name = JOptionPane.showInputDialog("Enter Your Name");
+      // JOptionPane.showMessageDialog(null, name);
+
+      // int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Your Age"));
+      // JOptionPane.showMessageDialog(null, "You are " + age + "Years Old");
+
+      // double height = Double.parseDouble(JOptionPane.showInputDialog("Enter Your
+      // Height"));
+      // JOptionPane.showMessageDialog(null, "You are " + height + "cm tall");
+      // GUI ends
+
+      // Hypotenuse start
+      // double x,y,z;
+      // Scanner scanner = new Scanner(System.in);
+
+      // System.out.println("Enter side x ");
+      // x = scanner.nextDouble();
+      // System.out.println("Enter side y ");
+      // y = scanner.nextDouble();
+      // z = Math.sqrt((x*x)+(y*y)); // hypotenuse formula
+      // System.out.println("The Hypotenuse is " + z);
+      // scanner.close();
+      // Hypotenuse end
+
+      // Generate Random Values start
+      // Random random = new Random();
+      // // int x = random.nextInt(6); // number b/w 0 to 5
+      // int x = random.nextInt(6)+1; // number b/w 1 to 6
+
+      // System.out.println(x);
+      // Generate Random Values end
+
+      // if statement start
+      // int age = 12;
+      // if(age>=18){
+      // System.out.println("Yor are adult");
+      // }
+      // else if(age<=18 && age >= 12){
+      // System.out.println("Yor are boy");
+      // }
+      // else{
+      // System.out.println("Yor are kid");
+      // }
+      // if statement end
 
 
-      System.out.println("Hello " + name);
-      System.out.println("Hello " + age);
-      System.out.println("Hello " + food);
+      // while start
+      // Scanner scanner = new Scanner(System.in);
+      // String name = "";
+      // while (name.isEmpty()) {
+      //    System.out.println("Enter your name");
+      //    name = scanner.nextLine();
+      // }
+      // System.out.println("Hello " + name);
+      // while end
+
+
+      // do start
+   //    Scanner scanner = new Scanner(System.in);
+   //    String name = "";
+   //   do {
+   //       System.out.println("Enter your name");
+   //       name = scanner.nextLine();
+   //    }while (name.isEmpty()) 
+   //    System.out.println("Hello " + name);
+      // do end
 
    }
 }
